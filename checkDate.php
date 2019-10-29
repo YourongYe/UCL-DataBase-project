bb22486234fbe8fd7aef14dedd664ce1b22e84cd
Yourong Ye <yourong.ye@hotmail.com>
2019-10-29 23:15:26 +0000

    Update checkDate.php

--- checkDate.php
+++ checkDate.php
@@ -3,10 +3,12 @@
   if (isset($_POST['submit'])) {
     if (!isset($datefield_startdate) || !isset($datefield_startdate)) {
       $error = "Please enter choose a startdate and enddate";
+      header("Location: index.php?error=" . urlencode($error));
       exit();
     }
     else ($datefield_startdate >= $datefield_enddate)) {
       $error = "Please enter valid startdate and enddate";
+      header("Location: index.php?error=" . urlencode($error));
       exit();
     }
   }
